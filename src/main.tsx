cb66fde20d8ac8c1b6d1a2d81f847a58ad4ee328
Juan Ceballos <jceballos.dev@outlook.com>
2024-11-01 16:34:21 -0500

    Eliminar la extensión de archivo de importación de App en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
+import App from './app';
 import './styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
